8adc1e9a7f630e45e457ec7cdc0e8b050e653272
aritode <arian2k@gmail.com>
2016-11-22 14:58:40 +0300

    added Module 01 - 01 ideal weight

---
+++ Module_01/01_ideal_weight.rb
@@ -0,0 +1,13 @@
+print 'Введите Ваше имя: '
+name = STDIN.gets.chomp
+
+print 'Введите Ваш рост: '
+height = STDIN.gets.to_i
+
+ideal_weight = height - 110
+
+if ideal_weight > 0
+  puts "#{name.capitalize}, Ваш идеальный вес - #{ideal_weight} кг."
+else
+  puts 'Ваш вес уже оптимальный'
+end
